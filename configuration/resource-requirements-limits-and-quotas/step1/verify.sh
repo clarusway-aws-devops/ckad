35825a8e872e09d6bbac73c3011843e930f9e1f3
james-clarusway <james@clarusway.com>
2023-07-29 17:52:22 +0300

    res-quota-1

--- configuration/resource-requirements-limits-and-quotas/step1/verify.sh
+++ configuration/resource-requirements-limits-and-quotas/step1/verify.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-if [[ $(kubectl get po -o yaml | grep '\- image:' | awk '{print $3}') = 'nginx:latest' \
-&& $(kubectl get po mypod -o yaml | grep "tier" | awk '{print $1,$2}') = 'tier: frontend' \
-&& $(kubectl get po mypod -o yaml | grep "mypod" | head -1 | awk '{print $2}') = 'mypod' ]]
+if [[ $(kubectl get po myapp -o jsonpath='{.spec.containers[0].resources.requests.cpu}') = '300m' \
+&& $(kubectl get po myapp -o jsonpath='{.spec.containers[0].resources.limits.cpu}') = '500m' ]]
 then
   exit 0
 else
