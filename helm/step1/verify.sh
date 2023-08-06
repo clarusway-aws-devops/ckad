16d6bbdb0f8e7088b724650339f7b95a8af1358a
james-clarusway <james@clarusway.com>
2023-08-06 13:00:07 +0300

    helm-2

--- helm/step1/verify.sh
+++ helm/step1/verify.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [[ $(helm get values mysql-release -o json | jq '.auth.rootPassword') = 'mysecretpassword' \
-&& $(helm get values mysql-release -o json | jq '.image.tag') = '8.0.26-debian-10-r68' \
-&& $(helm get values mysql-release -o json | jq '.mysqlDatabase') = 'mydatabase' \
-&& $(helm get values mysql-release -o json | jq '.mysqlPassword') = 'mypassword' \
-&& $(helm get values mysql-release -o json | jq '.mysqlUser') = 'myuser' ]]
+if [[ $(helm get values mysql-release -o json | jq '.auth.rootPassword') = '"mysecretpassword"' \
+&& $(helm get values mysql-release -o json | jq '.image.tag') = '"8.0.26-debian-10-r68"' \
+&& $(helm get values mysql-release -o json | jq '.mysqlDatabase') = '"mydatabase"' \
+&& $(helm get values mysql-release -o json | jq '.mysqlPassword') = '"mypassword"' \
+&& $(helm get values mysql-release -o json | jq '.mysqlUser') = '"myuser"' ]]
 then
   exit 0
 else
