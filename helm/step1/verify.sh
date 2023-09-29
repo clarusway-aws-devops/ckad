67d71108d7a29dc68106ba3c5855eeb0da15a917
james-clarusway <james@clarusway.com>
2023-09-29 19:44:29 +0300

    Helm question is modified

--- helm/step1/verify.sh
+++ helm/step1/verify.sh
@@ -2,9 +2,9 @@
 
 if [[ $(helm get values mysql-release -o json | jq '.auth.rootPassword') = '"mysecretpassword"' \
 && $(helm get values mysql-release -o json | jq '.image.tag') = '"8.0.26-debian-10-r68"' \
-&& $(helm get values mysql-release -o json | jq '.mysqlDatabase') = '"mydatabase"' \
-&& $(helm get values mysql-release -o json | jq '.mysqlPassword') = '"mypassword"' \
-&& $(helm get values mysql-release -o json | jq '.mysqlUser') = '"myuser"' ]]
+&& $(helm get values mysql-release -o json | jq '.auth.database') = '"mydatabase"' \
+&& $(helm get values mysql-release -o json | jq '.auth.password') = '"mypassword"' \
+&& $(helm get values mysql-release -o json | jq '.auth.user') = '"myuser"' ]]
 then
   exit 0
 else
