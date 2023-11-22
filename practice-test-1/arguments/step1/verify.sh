9cfebf5e6647bb4931e91096a343e4995b81cbcd
james-clarusway <james@clarusway.com>
2023-11-22 13:23:53 +0300

    practice-test-1 arg

--- practice-test-1/arguments/step1/verify.sh
+++ practice-test-1/arguments/step1/verify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ $(kubectl get po cw-cmdargs -o jsonpath='{.spec.containers[0].image}') = 'nginx' \
-&& $(kubectl get po cw-cmdargs -o jsonpath='{.spec.containers[0].args}') = '["/bin/sh","-c","i=0; while true; do echo \"$i: $(date)\" \u003e\u003e /var/log/first.log; echo \"$(date) INFO $i\" \u003e\u003e /var/log/second.log; i=$((i+1)); sleep 1; done"]' ]]
+if [[ $(kubectl get po cw-logging -o jsonpath='{.spec.containers[0].image}') = 'nginx' \
+&& $(kubectl get po cw-logging -o jsonpath='{.spec.containers[0].args}') = '["/bin/sh","-c","i=0; while true; do echo \"$i: $(date)\" \u003e\u003e /var/log/first.log; echo \"$(date) INFO $i\" \u003e\u003e /var/log/second.log; i=$((i+1)); sleep 1; done"]' ]]
 then
   exit 0
 else
