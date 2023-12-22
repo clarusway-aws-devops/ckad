82d97a4d675c8021f03f055f4535fb92bc23dbba
james-clarusway <james@clarusway.com>
2023-12-22 19:44:43 +0300

    docker-basics-6

--- docker/docker-basic/step3/verify.sh
+++ docker/docker-basic/step3/verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $(docker container inspect secondcon -f {{.Name}}) = $(cat /root/secondcon.txt) \
+if [[ $(docker container inspect secondcon -f {{.Id}}) = $(cat /root/secondcon.txt) \
 || $(docker container ls -a -f "name=secondcon" | awk '{print $1}' | tail -1) = $(cat /root/secondcon.txt) ]]
 then
   exit 0
