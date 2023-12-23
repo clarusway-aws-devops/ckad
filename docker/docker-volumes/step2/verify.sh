062c40c7f5eb3525a9eb42944dec5457ab5e822b
james-clarusway <james@clarusway.com>
2023-12-23 19:36:33 +0300

    docker volume is added-1

--- docker/docker-volumes/step2/verify.sh
+++ docker/docker-volumes/step2/verify.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+docker start alpha-con
 
 if [[ $(docker container inspect alpha-con -f {{.Name}}) = '/alpha-con' \
 && $(docker container inspect alpha-con -f {{.Config.Image}}) = 'alpine' \
-&& docker start alpha-con && $(docker exec alpha-con cat /alpha/myapp) = 'hello world' ]]
+&& $(docker exec alpha-con cat /alpha/myapp) = 'hello world' ]]
 then
   exit 0
 else
