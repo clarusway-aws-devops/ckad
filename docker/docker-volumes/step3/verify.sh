af08aaa8f8aa4ab500cb28b899f09aa19fdd4034
james-clarusway <james@clarusway.com>
2023-12-23 19:52:54 +0300

    docker volume is added-2

--- docker/docker-volumes/step3/verify.sh
+++ docker/docker-volumes/step3/verify.sh
@@ -2,7 +2,7 @@
 
 if [[ $(docker container inspect beta-con -f {{.Name}}) = '/beta-con' \
 && $(docker container inspect beta-con -f {{.Config.Image}}) = 'ubuntu' \
-&& $(docker container inspect beta-con --format='{{.HostConfig.Binds}}'}}) = '[myvolume:/beta:ro]' ]]
+&& $(docker container inspect beta-con --format='{{.HostConfig.Binds}}') = '[myvolume:/beta:ro]' ]]
 then
   exit 0
 else
