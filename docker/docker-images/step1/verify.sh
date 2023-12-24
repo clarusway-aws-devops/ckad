e39e733f56892fefa80510a0cdb9396a6ba41552
james-clarusway <james@clarusway.com>
2023-12-24 12:04:38 +0300

    docker image-3

--- docker/docker-images/step1/verify.sh
+++ docker/docker-images/step1/verify.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [[ $(docker image inspect myapp:latest -f {{.RepoTags}}) = '[myapp:latest myapp:v1]' \
-&& $(docker container inspect sun -f {{.Config.Image}}) = 'nginx' \
 && $(docker image inspect myapp:latest -f '{{json .Config}}' | jq -r '.Env[]' | grep '^APP_NAME=' | cut -d '=' -f 2) = 'mynewapp' ]]
 then
   exit 0
