6334ee8fae7c60150f1493386731c00740ad0c0e
james-clarusway <james@clarusway.com>
2023-12-24 13:42:20 +0300

    docker-image-7

--- docker/docker-images/step3/verify.sh
+++ docker/docker-images/step3/verify.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [[ $(docker container inspect mycontainer -f {{.Name}}) = '/mycontainer' \
-&& $(docker container inspect mycontainer -f {{.Config.Image}}) = 'localhost:5000/myapp:v1' ]]
+&& $(docker container inspect mycontainer -f {{.Config.Image}}) = 'localhost:5000/myapp:v1' \
+&& $(docker container inspect mycontainer -f '{{json .Config.Env}}' | jq -r '.[] | select(startswith("APP_NAME=")) | split("=")[1]') = 'myperfectapp' ]]
 then
   exit 0
 else
