18e36915845f7d7a8787da47fb7fba2e4c900632
james-clarusway <james@clarusway.com>
2023-12-24 12:23:00 +0300

    docker-image-5

--- docker/docker-images/step4/verify.sh
+++ docker/docker-images/step4/verify.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-if [[ $(docker container inspect mars -f {{.Name}}) = '/mars' \
-&& $(docker container inspect mars -f {{.Config.Image}}) = 'nginx' \
-&& $(docker container inspect mars --format='{{json .NetworkSettings}}' | jq -r '.Ports."80/tcp"[0].HostPort') = '9090' ]]
+if [[ $(docker container inspect mynewapp -f {{.Name}}) = '/mynewapp' ]]
 then
   exit 0
 else
