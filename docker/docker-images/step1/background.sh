0c962267452aef01af2f9a02cbcf0f46f897b882
james-clarusway <james@clarusway.com>
2023-12-25 10:23:53 +0300

    image-3

--- docker/docker-images/step1/background.sh
+++ docker/docker-images/step1/background.sh
@@ -5,6 +5,6 @@ echo starting... # to test stdout output in /var/log/killercoda
 
 mkdir -p docker-hub/data
 chmod 777 -R docker-hub/data
-docker-compose -d -f /root/docker-hub/docker-compose.yaml up
+docker-compose -f /root/docker-hub/docker-compose.yaml up -d
 
 touch /tmp/finished
