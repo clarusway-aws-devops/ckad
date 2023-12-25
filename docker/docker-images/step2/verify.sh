53187bb55c1939d65c68eab5e60a9b369bf2242d
james-clarusway <james@clarusway.com>
2023-12-25 13:24:08 +0300

    image-5

--- docker/docker-images/step2/verify.sh
+++ docker/docker-images/step2/verify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ $(docker image inspect localhost/myapp:v1 -f '{{json .RepoTags}}' | jq -r '.[]' | cut -d: -f2 | grep v1) = 'v1' \
-&& $(docker image inspect localhost/myapp:latest -f '{{json .RepoTags}}' | jq -r '.[]' | cut -d: -f2 | grep latest) = 'latest' \
+if [[ $(docker image inspect localhost/myapp:v1 -f '{{json .RepoTags}}' | jq -r '.[]' | cut -d: -f2 | grep v1 | head -1) = 'v1' \
+&& $(docker image inspect localhost/myapp:latest -f '{{json .RepoTags}}' | jq -r '.[]' | cut -d: -f2 | grep latest | head -1) = 'latest' \
 && $(ls -l /root/docker-hub/data/docker/registry/v2/repositories/myapp/_manifests/tags | grep -o latest) = 'latest' \
 && $(ls -l /root/docker-hub/data/docker/registry/v2/repositories/myapp/_manifests/tags | grep -o v1) = 'v1' ]]
 then
