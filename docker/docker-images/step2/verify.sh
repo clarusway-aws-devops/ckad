30fb03a1f47710261be0c4435eb201a50bb24b04
james-clarusway <james@clarusway.com>
2023-12-25 11:31:48 +0300

    image-4

--- docker/docker-images/step2/verify.sh
+++ docker/docker-images/step2/verify.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [[ $(docker image inspect localhost:5000/myapp:latest -f {{.RepoTags}}) = '[myapp:latest myapp:v1 localhost:5000/myapp:latest localhost:5000/myapp:v1]' ]]
+if [[ $(docker image inspect localhost:5000/myapp:latest -f {{.RepoTags}}) = '[myapp:latest myapp:v1 localhost:5000/myapp:latest localhost:5000/myapp:v1]' \
+&& $(ls /root/docker-hub/data/docker/registry/v2/repositories/myapp/_manifests/tags) = 'ls /root/docker-hub/data/docker/registry/v2/repositories/myapp/_manifests/tags' ]]
 then
   exit 0
 else
