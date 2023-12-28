a03c02121116f0c62551aed99a900bc10822853d
james-clarusway <james@clarusway.com>
2023-12-28 10:51:38 +0300

    docker-volume-2

--- docker/docker-volumes/step2/verify.sh
+++ docker/docker-volumes/step2/verify.sh
@@ -2,7 +2,7 @@
 docker start alpha-con
 
 if [[ $(docker container inspect alpha-con -f {{.Name}}) = '/alpha-con' \
-&& $(docker container inspect alpha-con -f {{.Config.Image}}) = 'nginx' \
+&& $(docker container inspect alpha-con -f {{.Config.Image}}) = 'alpine' \
 && $(docker cp alpha-con:/alpha /tmp > /dev/null && cat /tmp/alpha/myapp) = 'hello world' ]]
 then
   exit 0
