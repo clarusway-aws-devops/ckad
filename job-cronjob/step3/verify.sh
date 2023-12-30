ef0ea71fc5c1ebe121002aced1168f05c87a6bf8
james-clarusway <james@clarusway.com>
2023-12-30 16:31:33 +0300

    job-3

--- job-cronjob/step3/verify.sh
+++ job-cronjob/step3/verify.sh
@@ -2,8 +2,8 @@
 
 if [[ $(kubectl get cj hello-cronjob -o jsonpath='{.spec.schedule}') == '* * * * *' \
 && $(kubectl get cj hello-cronjob -o jsonpath='{.spec.jobTemplate.spec.template.spec.containers[0].image}') == 'busybox' \
-&& $(kubectl get cj hello-cronjob -o jsonpath='{.spec.successfulJobsHistoryLimit}') == '3' \
-&& $(kubectl get cj hello-cronjob -o jsonpath='{.spec.failedJobsHistoryLimit}') == '5' ]]
+&& $(kubectl get cj hello-cronjob -o jsonpath='{.spec.successfulJobsHistoryLimit}') == '5' \
+&& $(kubectl get cj hello-cronjob -o jsonpath='{.spec.failedJobsHistoryLimit}') == '3' ]]
 then
   exit 0
 else
