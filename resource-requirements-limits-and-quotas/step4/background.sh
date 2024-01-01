b533c7922ac8b8a6920a8d934e51df6011c9587f
james-clarusway <james@clarusway.com>
2024-01-01 14:01:24 +0300

    resource-limit-q4 is added

--- resource-requirements-limits-and-quotas/step4/background.sh
+++ resource-requirements-limits-and-quotas/step4/background.sh
@@ -13,13 +13,13 @@ metadata:
 spec:
   limits:
   - default:  # this section defines default limits
-      cpu: "1"
+      cpu: "500m"
       memory: "500Mi"
     defaultRequest: # this section defines default requests
-      cpu: "0.5"
+      cpu: "300m"
       memory: "300Mi"
     max:    # max and min define the limit range
-      cpu: "1.5"
+      cpu: "1"
       memory: "800Mi"
     min:
       cpu: "100m"
