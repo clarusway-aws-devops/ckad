9c9a72efee81a928f2bf59a1b9b2f7f34162002a
james-clarusway <james@clarusway.com>
2024-02-14 19:46:09 +0300

    correction typo

--- common-deployment-strategies/blue-green/step1/verify.sh
+++ common-deployment-strategies/blue-green/step1/verify.sh
@@ -2,7 +2,6 @@
 
 if [[ $(kubectl get deploy clarusweb-v2 -o jsonpath='{.spec.template.spec.containers[0].image}') = 'clarusway/clarusweb:2.0' \
 && $(kubectl get deploy clarusweb-v2 -o jsonpath='{.spec.replicas}') -eq 3 \
-&& $(kubectl get deploy clarusweb-v2 -o jsonpath='{.spec.template.metadata.labels.app}') = 'clarusweb' \
 && $(kubectl get deploy clarusweb-v2 -o jsonpath='{.spec.template.metadata.labels.version}') = 'v2' \
 && $(curl -s localhost:30001/ | grep -o  'Version 2') = 'Version 2' ]]
 then
