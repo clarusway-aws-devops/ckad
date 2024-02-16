c97ef988de1c790338a7774907b152db615aeccb
james-clarusway <james@clarusway.com>
2024-02-16 11:15:39 +0300

    netpol question is modified

--- networking/networkpolicy/step1/verify.sh
+++ networking/networkpolicy/step1/verify.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $(kubectl get netpol nginx-netpol -o jsonpath='{.spec.podSelector.matchLabels}' | grep -o '"tier":"frontend"') == '"tier":"frontend"' && \
-$(kubectl get netpol nginx-netpol -o jsonpath='{.spec.ingress[0].from[*].podSelector.matchLabels}' | grep -o '"role":"frontend"') == '"role":"frontend"' ]] && \
-$(kubectl get netpol nginx-netpol -o jsonpath='{.spec.ingress[0].ports[0].port}') -eq 80 && \
-$(kubectl get netpol nginx-netpol -o jsonpath='{.spec.egress[0].ports[*].port}') == '53 53'
+$(kubectl get netpol nginx-netpol -o jsonpath='{.spec.ingress[0].from[*].podSelector.matchLabels}' | grep -o '"role":"frontend"') == '"role":"frontend"' && \
+$(kubectl get netpol nginx-netpol -o jsonpath='{.spec.ingress[0].ports[0].port}') == '80' && \
+$(kubectl get netpol nginx-netpol -o jsonpath='{.spec.egress[0].ports[*].port}') == '53 53' ]]
 
 then
   exit 0
