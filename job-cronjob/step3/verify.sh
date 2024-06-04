7f879f440fc51949e15cf91b61416a142298b323
james-clarusway <james@clarusway.com>
2024-06-04 08:57:29 +0300

    correction typo

--- job-cronjob/step3/verify.sh
+++ job-cronjob/step3/verify.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [[ $(kubectl get cj hello-cronjob -o jsonpath='{.spec.schedule}') == '* * * * *' \
+|| $(kubectl get cj hello-cronjob -o jsonpath='{.spec.schedule}') == '*/1 * * * *' \
 && $(kubectl get cj hello-cronjob -o jsonpath='{.spec.jobTemplate.spec.template.spec.containers[0].image}') == 'busybox' \
 && $(kubectl get cj hello-cronjob -o jsonpath='{.spec.successfulJobsHistoryLimit}') == '5' \
 && $(kubectl get cj hello-cronjob -o jsonpath='{.spec.failedJobsHistoryLimit}') == '3' ]]
