f5b557df5a02bad6afc2968078a70ecc731a3ba9
james-clarusway <james@clarusway.com>
2024-06-04 09:17:23 +0300

    correction typo

--- basic-consepts/step1/verify.sh
+++ basic-consepts/step1/verify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $(kubectl get po mypod -o jsonpath='{.spec.containers[0].image}') = 'nginx:latest' \
-&& $(kubectl get po mypod -o jsonpath='{.metadata.labels}' | grep -o '"tier":"frontend"') = '"tier":"frontend"}' ]]
+&& $(kubectl get po mypod -o jsonpath='{.metadata.labels}' | grep -o '"tier":"frontend"') = '"tier":"frontend"' ]]
 then
   exit 0
 else
