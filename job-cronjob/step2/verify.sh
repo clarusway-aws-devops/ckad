37a8c7e13168ecefdf174775cf3c7385071a323a
james-clarusway <james@clarusway.com>
2024-06-04 09:00:54 +0300

    correction typo

--- job-cronjob/step2/verify.sh
+++ job-cronjob/step2/verify.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [[ $( kubectl get job hello-job -o jsonpath='{.spec.completions}') -eq 6 \
-&& $(kubectl get job hello-job -o jsonpath='{.spec.parallelism}') -eq 2 \
-&& $(kubectl get jobs | grep hello-job | awk '{print $3}') == '6/6' ]]
+&& $(kubectl get job hello-job -o jsonpath='{.spec.parallelism}') -eq 2 ]]
 then
   exit 0
 else
