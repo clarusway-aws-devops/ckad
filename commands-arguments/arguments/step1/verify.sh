b77366c248d98abf5628a4ca965fadd9603678c7
james-clarusway <james@clarusway.com>
2024-06-18 07:34:09 +0300

    command-arg correction typo

--- commands-arguments/arguments/step1/verify.sh
+++ commands-arguments/arguments/step1/verify.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if [[ $(kubectl get po cw-logging -o jsonpath='{.spec.containers[0].image}') = 'nginx' \
-&& $(kubectl get po cw-logging -o jsonpath='{.spec.containers[0].args}') = '["/bin/sh","-c","i=0; while true; do echo \"$i: $(date)\" \u003e\u003e /var/log/first.log; echo \"$(date) INFO $i\" \u003e\u003e /var/log/second.log; i=$((i+1)); sleep 1; done"]' ]]
+&& $(kubectl get po cw-logging -o jsonpath='{.spec.containers[0].args}') = '["/bin/sh","-c","i=0; while true; do echo \"$i: $(date)\" \u003e\u003e /var/log/first.log; echo \"$(date) INFO $i\" \u003e\u003e /var/log/second.log; i=$((i+1)); sleep 1; done"]' \
+|| $(kubectl get po cw-logging -o jsonpath='{.spec.containers[0].command}') = '["/bin/sh"]' \
+&& $(kubectl get po cw-logging -o jsonpath='{.spec.containers[0].args}') = '["-c","i=0; while true; do echo \"$i: $(date)\" \u003e\u003e /var/log/first.log; echo \"$(date) INFO $i\" \u003e\u003e /var/log/second.log; i=$((i+1)); sleep 1; done"]'
+]]
 then
   exit 0
 else
